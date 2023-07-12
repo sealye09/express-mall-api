c335e5d8176e4d0c1baba934dfc0dd3ef3e77d13
seal <yejunhai2001@gmail.com>
2023-07-12 09:10:22 +0800

    #

--- app.js
+++ app.js
@@ -1,6 +1,5 @@
 import express from "express";
 import bodyParser from "body-parser";
-import mongoose from "mongoose";
 import cors from "cors";
 import helmet from "helmet";
 import morgan from "morgan";
@@ -8,17 +7,16 @@ import dotenv from "dotenv";
 import swaggerUi from "swagger-ui-express";
 import YAML from "yamljs";
 
+import { dbConnext } from "./config/db.js";
+
 // routes import
 import routes from "./routes/index.js";
+import User from './models/User.js';
 
 /* 配置 */
 dotenv.config();
 const app = express();
-
 const swaggerDocument = YAML.load("./swagger.yaml");
-
-// 配置Swagger UI路由
-app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 app.use(express.json());
 app.use(helmet());
@@ -29,24 +27,14 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cors());
 
 /* 路由 */
+// 配置Swagger UI路由
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 app.use("/", routes);
 
 /* 数据库 */
 const PORT = process.env.PORT || 3003;
-mongoose
-  .connect(process.env.MONGO_URL, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  })
+dbConnext()
   .then(() => {
     app.listen(PORT, () => console.log(`Server Port: ${PORT}`));
-
-    /* ONLY ADD DATA ONE TIME */
-    // AffiliateStat.insertMany(dataAffiliateStat);
-    // OverallStat.insertMany(dataOverallStat);
-    // Product.insertMany(dataProduct);
-    // ProductStat.insertMany(dataProductStat);
-    // Transaction.insertMany(dataTransaction);
-    // User.insertMany(dataUser);
   })
   .catch((error) => console.log(`${error} did not connect`));
